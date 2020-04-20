fedd96ad16b026056871c692f4d48a30cf4fbfed
hoomch <hoomonchamani@yahoo.com>
2020-04-20 19:21:05 +0430

    init commit

---
+++ serial.hpp
@@ -0,0 +1,28 @@
+#include <string>
+#include <iostream>
+#include <vector>
+#include <sstream>
+#include <iomanip>
+#include <fstream>
+#include <bits/stdc++.h> 
+#include <typeinfo>
+#include <stdio.h>
+#include <sys/types.h>
+#include <dirent.h>
+#include <cstring>
+#include <unistd.h>
+#include <list>
+#include <sys/wait.h>
+#include <sys/stat.h>
+#include <fstream>
+#include <iosfwd>
+#include <string.h>
+#include <fcntl.h>
+#include <map>
+#include <algorithm>
+
+
+
+
+
+using namespace std;
