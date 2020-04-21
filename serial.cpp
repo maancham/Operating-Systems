48a8ce3402bc12a25d9583118ec60396d0b4d2eb
hoomch <hoomonchamani@yahoo.com>
2020-04-21 23:35:34 +0430

    cleaned a little

--- serial.cpp
+++ serial.cpp
@@ -44,6 +44,30 @@ void print_output(vector<string> winner, double final_points)
     cout << final_points << endl;
 }
 
+void get_revs_data(vector<vector<string> > revs_data, vector<double> &points, 
+                       vector<double> &num_of_likes, unordered_map<string, int> id_to_index)
+{
+    for(int i=0; i<revs_data.size(); i++)
+    {
+        if(id_to_index.find(revs_data[i][0]) != id_to_index.end())
+        {
+            double x,y;
+            int index = id_to_index.find(revs_data[i][0])->second;
+            stringstream geek(revs_data[i][1]); 
+            stringstream seek(revs_data[i][2]);
+            geek >> x;
+            seek >> y;
+            points[index] += x*y;
+            num_of_likes[index] += y;
+        }
+    }
+}
+
+int find_max_index(vector<double> &points)
+{
+    return distance(points.begin(), max_element (points.begin(),points.end()));
+}
+
 vector<string> extract_winner(vector<vector<string> > books_data, int row_number)
 {
     vector<string> winner;
@@ -60,6 +84,7 @@ void find_top(vector<vector<string> > books_data, vector<vector<string> > revs_d
     vector<vector<string> > candidates;
     vector<double> points;
     vector<double> num_of_likes;
+
     for(int i=0; i<books_data.size(); i++)
     {
         if ((books_data[i][2] == genre) || (books_data[i][3] == genre))
@@ -76,20 +101,7 @@ void find_top(vector<vector<string> > books_data, vector<vector<string> > revs_d
         }
     }
 
-    for(int i=0; i<revs_data.size(); i++)
-    {
-        if(id_to_index.find(revs_data[i][0]) != id_to_index.end())
-        {
-            double x,y;
-            int index = id_to_index.find(revs_data[i][0])->second;
-            stringstream geek(revs_data[i][1]); 
-            stringstream seek(revs_data[i][2]);
-            geek >> x;
-            seek >> y;
-            points[index] += x*y;
-            num_of_likes[index] += y;
-        }
-    }
+    get_revs_data(revs_data, points, num_of_likes, id_to_index);
 
     for(int i=0; i<points.size(); i++)
     {
@@ -101,7 +113,7 @@ void find_top(vector<vector<string> > books_data, vector<vector<string> > revs_d
         points[i] /= TEN;
     }
 
-    int max_index = distance(points.begin(), max_element (points.begin(),points.end()));
+    int max_index = find_max_index(points);
     stringstream geek(candidates[max_index][3]);
     geek >> row_number;
 
@@ -116,12 +128,8 @@ int main(int argc, char **argv)
 {
     string genre = argv[1];
     
-    
     vector<vector<string> > books_data = parse_csv(BOOKS_FILE);
     vector<vector<string> > revs_data = parse_csv(REVS_FILE);
 
-
     find_top(books_data, revs_data, genre);
-    
-
 }
